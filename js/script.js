17ff9a4bf9df963e7cdcde3d5c7e5bd00559ad43
Gigi Frias <gigi.frias@gmail.com>
2019-03-18 18:00:32 -0400

    Adding buttons for each year, updating styles, excel spreadsheet with countries and years

--- js/script.js
+++ js/script.js
@@ -9,8 +9,10 @@ var pymChild = new pym.Child();
 // DEFINE VARIABLES
 // Define size of map group
 // Full world map is 2:1 ratio
-w = 3000;
-h = 1500;
+/*w = 3000;
+h = 1500;*/
+var w = parseInt(d3.select("#master_container").style("width")),
+	h = w / 2;
 // variables for catching min and max zoom factors
 var minZoom;
 var maxZoom;
@@ -19,7 +21,7 @@ var maxZoom;
 // Define map projection
 var projection = d3
 	.geoMercator()
-	.center([0, 15]) // set centre to further North as we are cropping more off bottom of map
+	.center([0, 20]) // set centre to further North as we are cropping more off bottom of map
 	.scale([w / (2 * Math.PI)]) // scale to fit group width
 	.translate([w / 2, h / 2]) // ensure centered in group
 ;
@@ -124,6 +126,7 @@ $(window).resize(function() {
 var svg = d3
 	.select("#map")
 	.append("svg")
+	.attr("id", "map_container")
 	// set to the same size as the "map-holder" div
 	.attr("width", $("#map").width())
 	.attr("height", $("#map").height())
